30698700441fc8e48fdbc2351162dd025a57e2e3
Nick Sourvanos <nikolaos.sourvanos@atherasanalytics.com>
2023-10-04 20:51:11 +0000

    Finish database optimization

--- blango/urls.py
+++ blango/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.urls import path, include
+
+import debug_toolbar
 import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("ip/", blog.views.get_ip),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
