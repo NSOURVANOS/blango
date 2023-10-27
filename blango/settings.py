1b2135dfc54634a73d4d8ebb1e73e789d260b351
Nick Sourvanos <nikolaos.sourvanos@atherasanalytics.com>
2023-10-27 20:16:11 +0000

    Finish pagination and django filter

--- blango/settings.py
+++ blango/settings.py
@@ -90,6 +90,7 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'rest_framework',
         'rest_framework.authtoken',
+        'django_filters',
         'crispy_forms',
         'crispy_bootstrap5',
         'debug_toolbar',
@@ -121,6 +122,12 @@ class Dev(Configuration):
             "user_sustained": "5000/day",
             "user_burst": "100/minute",
         },
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 100,
+        "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend",
+            "rest_framework.filters.OrderingFilter"
+        ],
     }
 
     MIDDLEWARE = [
